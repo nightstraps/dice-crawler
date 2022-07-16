abfdc34687e3c4e8f44506b33a377546255b0d8a
groobob <91916133+groobob@users.noreply.github.com>
2022-07-16 14:51:46 -0400

    title working

---
+++ Assets/Scripts/sceneLoader.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneLoader : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex += 1);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
