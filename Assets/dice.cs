0f3e3924a0645f5d59413710f6a8294e22ae65e0
Juno <nightstraps@gmail.com>
2022-07-16 13:53:14 -0400

    dice mechanic WIP

---
+++ Assets/dice.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dice : MonoBehaviour
+{
+    //0 represents x, 1 represents x and so on. This helps track what number is equal to what effect name
+    enum effectAlias{EFFECT1, EFFECT2};
+
+    int sides;
+    int maxEffects;
+    int[] effects;
+     
+    //generic dice constructor
+    public dice()
+    {
+        sides = 6;
+        maxEffects = 3;
+        effects = new int[maxEffects];
+        for (int i=0; i<maxEffects; i++){ effects[i] = -1;}
+    }
+
+    //better dice constructor
+    public dice(int sides, int maxEffects)
+    {
+        sides = this.sides;
+        maxEffects = this.maxEffects;
+        effects = new int[maxEffects];
+        //-1 represents no effects in that slot
+        for (int i=0; i<maxEffects; i++){ effects[i] = -1;}
+    }
+
+    public void debugDice()
+    {
+        Debug.Log("number of sides: " + sides + "\n");
+        Debug.Log("max number of effects "+ maxEffects + "\n");
+        Debug.Log("contains the following number of effects: ");
+        for (int i=0; i<maxEffects; i++)
+        {
+            if (effects[i] != -1)
+            {
+                Debug.Log((effectAlias)effects[i]);
+            } 
+            else 
+            {
+                Debug.Log(", ");
+            }
+        }
+    }
+
+    //add an effect (it takes int for now, future plans for it to take name of effect instead, and use enum to turn into int)
+    public void addEffect(int effect)
+    {
+        for (int i=0; i<maxEffects; i++)
+        {
+            if (effects[i] == -1)
+            {
+                effects[i] = effect;
+                break;
+            }
+        }
+    }
+}
--- Assets/diceMaster.cs
+++ Assets/diceMaster.cs
@@ -4,26 +4,71 @@ using UnityEngine;
 
 public class diceMaster : MonoBehaviour
 {
-    public class Dice
+    public class dice
     {
-        public int sides;
+        //0 represents x, 1 represents x and so on. This helps track what number is equal to what effect name
+        enum effectAlias{EFFECT1, EFFECT2};
+
+        int sides;
         int maxEffects;
         int[] effects;
-
-        public Dice(int sides, int maxEffects)
+        
+        //generic dice constructor
+        public dice()
         {
             sides = 6;
             maxEffects = 3;
             effects = new int[maxEffects];
+            for (int i=0; i<maxEffects; i++){ effects[i] = -1;}
+        }
 
+        //better dice constructor
+        public dice(int sides, int maxEffects)
+        {
+            this.sides = sides;
+            this.maxEffects = maxEffects;
+            effects = new int[maxEffects];
+            //-1 represents no effects in that slot
+            for (int i=0; i<maxEffects; i++){ effects[i] = -1;}
+        }
+
+        public void debugDice()
+        {
+            Debug.Log("number of sides: " + sides + "\n");
+            Debug.Log("max number of effects "+ maxEffects + "\n");
+            Debug.Log("contains the following number of effects: ");
             for (int i=0; i<maxEffects; i++)
             {
-                effects[i] = 0;
+                if (effects[i] != -1)
+                {
+                    Debug.Log((effectAlias)effects[i]);
+                } 
+                else 
+                {
+                    Debug.Log(", ");
+                }
             }
         }
 
+        //TODO: addeffect overflow error handling. behaviour?
+        //add an effect (it takes int for now, future plans for it to take name of effect instead, and use enum to turn into int)
         public void addEffect(int effect)
         {
+            for (int i=0; i<maxEffects; i++)
+            {
+                if (effects[i] == -1)
+                {
+                    effects[i] = effect;
+                    break;
+                }
+            }
         }
+    }
+
+    void Start() 
+    {
+        dice x = new dice(6, 3);
+        x.addEffect(1);
+        x.debugDice(); 
     }
 }
